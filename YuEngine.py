09da6be5291e3d214e47cff4d11a8ca99e0e1dd0
yukaishou <18977786220@163.com>
2025-10-19 13:12:09 +0800

    增加了日志保存功能

--- YuEngine.py
+++ YuEngine.py
@@ -2,5 +2,7 @@ from engine import main as engine
 from engine.core import game_object as GameObject
 if __name__ == "__main__":
     game = engine.engine((800,600),60,"YuEngine")
+    game.log.save_log()
 else:
     game = engine.engine((800,600),60,"YuEngine")
+    game.log.save_log()
